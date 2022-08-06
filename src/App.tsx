1d3cebc86f56e61e0e0a0689f954ec9ba150a1d6
GiriPai <giripaiu@outlook.com>
2022-08-06 16:49:19 +0530

    adding img search component to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import DefaultLayout from "./components/layouts/DefaultLayout"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ImgSearchEngine from "./pages/ImgSearchEngine";
 
 const App = () => {
   return (
     <DefaultLayout>
-      <div>HELLO</div>
+      <ImgSearchEngine />
     </DefaultLayout>
   );
 }
